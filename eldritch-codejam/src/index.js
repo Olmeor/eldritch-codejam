91513185cd4715b4a22a988a731da94ea2348f9e
Mikhail Oleynikov (@Olmeor) <mn.oleynikov@gmail.com>
2022-08-23 15:20:38 +0700

    feat: add index.js

---
+++ eldritch-codejam/src/index.js
@@ -0,0 +1,12 @@
+// https://codejam-demo.netlify.app/
+// https://codejam-demo.herokuapp.com/
+
+import ancientsCards from './assets/Ancients';
+import blueCards from './assets/MythicCards/blue/index';
+import brownCards from './assets/MythicCards/brown/index';
+import greenCards from './assets/MythicCards/green/index';
+import difficulties from './data/difficulties';
+import ancientsData from './data/ancients';
+import blueCardsData from './data/mythicCards/blue';
+import brownCardsData from './data/mythicCards/brown';
+import greenCardsData from './data/mythicCards/green';
