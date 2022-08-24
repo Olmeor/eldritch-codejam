04438e445b9dfa1da4aa5ca54734b4a74ac2f6bd
Mikhail Oleynikov (@Olmeor) <mn.oleynikov@gmail.com>
2022-08-25 01:25:31 +0700

    feat: add chosens ancient anf difficulty

--- eldritch-codejam/src/js/index.js
+++ eldritch-codejam/src/js/index.js
@@ -1,13 +1,13 @@
-// https://codejam-demo.netlify.app/
-// https://codejam-demo.herokuapp.com/
-import '../css/style.css'
-
-import ancients from "../assets/Ancients/index";
-import ancientsData from "../data/ancients.js"
+import '../css/style.css';
+import ancientsList from "../assets/Ancients/index";
+import ancients from "../data/ancients.js"
 import difficulties from "../data/difficulties.js"
 import { brownCards, blueCards, greenCards } from "../data/mythicCards/index.js"
 import bg from "../assets/home.jpg";
 import cardBack from "../assets/mythicCardBackground.jpg"
+// console.log(ancients)
+
+// Верстка
 
 function setBackground () {
   const body = document.body;
@@ -28,26 +28,47 @@ function removeCardBack() {
 
 function setAncients() {
   const ancientCards = document.querySelector('.ancients-container');
-  ancientCards.children[0].style.backgroundImage = `url(${ancients.azathoth})`;
-  ancientCards.children[1].style.backgroundImage = `url(${ancients.cthulhu})`;
-  ancientCards.children[2].style.backgroundImage = `url(${ancients.iogSothoth})`;
-  ancientCards.children[3].style.backgroundImage = `url(${ancients.shubNiggurath})`;
+  ancientCards.children[0].style.backgroundImage = `url(${ancientsList.azathoth})`;
+  ancientCards.children[1].style.backgroundImage = `url(${ancientsList.cthulhu})`;
+  ancientCards.children[2].style.backgroundImage = `url(${ancientsList.iogSothoth})`;
+  ancientCards.children[3].style.backgroundImage = `url(${ancientsList.shubNiggurath})`;
+  console.log(Object.keys(ancientsList)[0])
 }
 setAncients();
 
-const ancientCards = document.querySelectorAll('.ancient-card');
+// Настройка колоды
+
+let activeAncient;
+let activeDifficulty;
+
+// Выбор древнего
+
+const ancientCard = document.querySelectorAll('.ancient-card');
+
 function choiceAncient(e) {
   const currentCard = e.currentTarget;
-  ancientCards.forEach(card => {
+  ancientCard.forEach(card => {
     card.classList.remove('active');
   });
   currentCard.closest('.ancient-card').classList.add('active');
+  setActiveAncient();
 }
-ancientCards.forEach(card => {
+
+function setActiveAncient() {
+  const ancientCards = document.querySelector('.ancients-container');
+  let activeItem = ancientCards.querySelector('.ancient-card.active').dataset.ancient;
+  activeAncient = ancients.find(item => item.name == activeItem);
+  console.log('выбран древний -', activeAncient)
+}
+
+ancientCard.forEach(card => {
   card.addEventListener('click', choiceAncient);
 });
 
+// Выбор сложности
+
 const difficultyLevels = document.querySelectorAll('.difficulty');
+
 function choiceDifficulty(e) {
   const currentDifficulty = e.currentTarget;
 
@@ -55,10 +76,20 @@ function choiceDifficulty(e) {
     level.classList.remove('active');
   });
   currentDifficulty.closest('.difficulty').classList.add('active');
+  setActiveDifficulty()
 }
+
+function setActiveDifficulty() {
+  const allDifficulty = document.querySelector('.difficulty-container');
+  activeDifficulty = allDifficulty.querySelector('.difficulty.active').dataset.difficulty;
+  console.log('выбрана сложность -', activeDifficulty)
+}
+
 difficultyLevels.forEach(level => {
   level.addEventListener('click', choiceDifficulty);
 });
+
+// Замешиваем колоду
 
 function getRandomNum(max){
   return Math.floor((Math.random() * max));
